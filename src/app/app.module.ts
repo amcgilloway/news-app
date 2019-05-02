529f7a8f5a003f312b4e1d705844afbe7566e10f
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2019-05-02 14:44:27 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {NewsApiService } from './news-api.service';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +14,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
   ],
-  providers: [],
+  providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/news-api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  api_key = '48d6d9557ada4249b2c16c0ac3e73c1d';
+
+  constructor(private http:HttpClient) { }
+
+  initSources(){
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key)
+  }
+
+  initArticles(){
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+
+  getArticlesByID(source: String){
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+}
